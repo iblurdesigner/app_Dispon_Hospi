773887ac13701fcd3115c2f54c4817531eb90ca9
iblurdesigner <nevomen@gmail.com>
2020-07-10 16:47:08 -0500

    first commit

---
+++ lib/src/Hospitales/ui/widgets/header_search.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HeaderSearch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    //throw UnimplementedError();
+
+   return Column(
+     children: <Widget>[
+       Container(
+         child: Image.asset(
+             'assets/img/header@2x.png'
+         ),
+       )
+     ],
+   );
+
+
+  }
+
+
+
+}
