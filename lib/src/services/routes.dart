f649a54a6131118df134e0e330f7b7b726e6c542
lenarvaez <lenarvaez@utpl.edu.ec>
2020-08-02 11:36:27 -0500

    Routes to evaluated & evaluation controller backend
    - POST
       - UpdateEvaluated
       - Evaluation
    - Classes
       - EvaluatedData
       - EvaluationData

--- lib/src/services/routes.dart
+++ lib/src/services/routes.dart
@@ -7,16 +7,73 @@ class Routes {
 
   static final String apiServer = 'http://10.0.2.2:3000';
   
-  static Future<http.Response> postEvaluated (String city, String address, String age) async {
+  static Future<http.Response> postEvaluated(String city, String address, String age) async {
     var url = apiServer + '/api/evaluated/save';
     Map data = {'ciudad':city,'dirEvaluado':address,'edad':age};
     debugPrint(json.encode(data));
     var response = await http.post(url, headers: {"ContectType": "application/x-www-form-urlencoded"}, body: data);
     return response;
   }
+
+  static Future<http.Response> postUpdateEvaluated(EvaluatedData evaluatedData) async {
+    var url = apiServer + '/api/evaluated/update';
+    Map data = {'idEvaluado': evaluatedData.idEvaluated.toString(), 'telefono': evaluatedData.phone };
+    debugPrint(json.encode(data));
+    var response = await http.post(url, headers: {"ContectType": "application/x-www-form-urlencoded"}, body: data);
+    return response;
+  }
+
+  static Future<http.Response> postEvaluation(EvaluationData evaluationData) async {
+    var url = apiServer + '/api/evaluation/save';
+    Map data = {
+      'pColoracionDedos': evaluationData.fingers, 
+      'pDificultadRespirar': evaluationData.breathe,
+      'pfiebre': evaluationData.fever, 
+      'pHipertenso': evaluationData.hypertensive,
+      'idEvaluado': evaluationData.idEvaluated.toString(),
+      'sumEvaluacion': evaluationData.sum.toString()
+    };
+    debugPrint(json.encode(data));
+    var response = await http.post(url, headers: {"ContectType": "application/x-www-form-urlencoded"}, body: data);
+    return response;
+  }
 }
 
-class DataEvaluated {
+class EvaluatedData {
   int idEvaluated;
-  DataEvaluated({this.idEvaluated});
+  String phone;
+  EvaluatedData({this.idEvaluated, this.phone});
+}
+
+class EvaluationData {
+  String fingers;
+  String breathe;
+  String fever;
+  String hypertensive;
+  int idEvaluated;
+  int sum = 0;
+  int idEvaluation;
+  EvaluationData({this.fingers, this.breathe, this.fever, this.hypertensive, this.idEvaluated});
+  
+  setData(String fingers, String breathe, String fever, String hypertensive, int idEvaluated) {
+    this.fingers = fingers;
+    this.breathe = breathe;
+    this.fever = fever;
+    this.hypertensive = hypertensive;
+    this.idEvaluated = idEvaluated;
+
+    sum = 0;
+    if (this.fingers=='Sí')
+      this.sum++;
+    if (this.breathe=='Sí')
+      this.sum++;
+    if (this.fever=='Sí')
+      this.sum++;
+    if (this.hypertensive=='Sí')
+      this.sum++;
+  }
+
+  setIdEvaluation(int idEvaluation) {
+    this.idEvaluation = idEvaluation;
+  }
 }
